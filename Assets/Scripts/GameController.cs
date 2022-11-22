e6b18c8c4ce5867fa169a66e6d2c5d5ca37c6368
iToastMost <chevre94@gmail.com>
2022-11-21 22:32:29 -0500

    Finished mechanics of game
    Mechanics finished. Could add win condition, different enemy types, additional towers, tower upgrades, more levels, death sounds, shooting sounds music etc.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExitApplication();   
+    }
+
+    void ExitApplication ()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
